03a1e651ba4c7f094e4826f689eabccb510b292e
trohini31383 <53841712+trohini31383@users.noreply.github.com>
2019-08-11 17:43:49 -0700

    changed the types for pokemons and removed the var pokemon from all objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 
 var  repository= [
-    var pokemon1={
+    {
         name: 'Charizard',
         height: 5
-      var types= ['Fire','Grass','Rock'];
+        types: ['Fire','Flying']
     };
-    var pokemon2={
+    {
         name:'Mewtwo',
         height: 6
-      var type=['Psychic','Water','Steel'];
+        type: ['Psychic'}
     };
-    var pokemon3={
+    {
         name: 'Sceptile',
         height:5
-      var type=['Grass', 'Fire','Water'];
+       type: ['Grass', 'poison']
     };
-    ];
+  ];
