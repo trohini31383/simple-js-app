288a8cfa148127db7039f0e12eb14c56c023bf03
trohini31383 <53841712+trohini31383@users.noreply.github.com>
2019-08-11 22:07:00 -0700

    corrected putting commas in place of semicolon.

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,17 @@
 var  repository= [
     {
         name: 'Charizard',
-        height: 5
+        height: 5,
         types: ['Fire','Flying']
-    };
+    },
     {
         name:'Mewtwo',
-        height: 6
+        height: 6,
         type: ['Psychic'}
-    };
+    },
     {
         name: 'Sceptile',
-        height:5
+        height:5,
        type: ['Grass', 'poison']
-    };
+    },
   ];
