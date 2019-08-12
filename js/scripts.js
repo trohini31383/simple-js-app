a4890e63d30c4b12a648d28390ea185b524870f3
trohini31383 <53841712+trohini31383@users.noreply.github.com>
2019-08-11 17:27:20 -0700

    changed the pokemon types to array of strings.

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,17 @@
 var  repository= [
     var pokemon1={
         name: 'Charizard',
-        height: 5.07
-        type: 'Fire',
+        height: 5
+      var types= ['Fire','Grass','Rock'];
     };
     var pokemon2={
         name:'Mewtwo',
-        height: 6.07
-        type:'Psychic',
+        height: 6
+      var type=['Psychic','Water','Steel'];
     };
     var pokemon3={
         name: 'Sceptile',
-        height:5.07
-        type: 'Grass',
+        height:5
+      var type=['Grass', 'Fire','Water'];
     };
     ];
