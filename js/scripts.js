d97f1223f71bf01e508d01fcfe03adc94e57f32f
trohini31383 <53841712+trohini31383@users.noreply.github.com>
2019-08-11 17:07:00 -0700

    Added new repostory for different pokemons.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
 
-alert('Hello World');
-var favoritefood='pizza';
-document.write(favoritefood)
+var  repository= [
+    var pokemon1={
+        name: 'Charizard',
+        height: 5.07
+        type: 'Fire',
+    };
+    var pokemon2={
+        name:'Mewtwo',
+        height: 6.07
+        type:'Psychic',
+    };
+    var pokemon3={
+        name: 'Sceptile',
+        height:5.07
+        type: 'Grass',
+    };
+    ];
